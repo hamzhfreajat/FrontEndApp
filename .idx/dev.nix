e79402fe7b4464f43cfce113cfb5166ea0ea7f64
Hamza Fraijat <hfraijat@optimizasolutions.com>
2026-06-09 10:26:33 +0300

    chore: configure Firebase Studio previews including iOS

---
+++ .idx/dev.nix
@@ -0,0 +1,27 @@
+{ pkgs, ... }: {
+  channel = "stable-24.05";
+  packages = [
+    pkgs.flutter
+  ];
+  idx = {
+    extensions = [
+      "dart-code.flutter"
+      "dart-code.dart-code"
+    ];
+    previews = {
+      enable = true;
+      previews = {
+        web = {
+          command = ["flutter" "run" "--machine" "-d" "web-server" "--web-hostname" "0.0.0.0" "--web-port" "$PORT"];
+          manager = "flutter";
+        };
+        android = {
+          manager = "flutter";
+        };
+        ios = {
+          manager = "flutter";
+        };
+      };
+    };
+  };
+}
