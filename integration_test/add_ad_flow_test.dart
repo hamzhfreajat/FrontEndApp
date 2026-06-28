ad2f3dd63c1f223afbe67b0fc146ec4f0f5bb580
Hamza Fraijat <hfraijat@optimizasolutions.com>
2026-06-28 16:59:43 +0300

    Fix import statement package name in E2E test

--- integration_test/add_ad_flow_test.dart
+++ integration_test/add_ad_flow_test.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
-import 'package:classifieds_app/main.dart' as app;
+import 'package:classifieds_frontend/main.dart' as app;
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter/services.dart';
 import 'package:path_provider/path_provider.dart';
