886cfcc67b41ee53541985ed9b63fd9d506c8af1
Hamza Fraijat <hfraijat@optimizasolutions.com>
2026-09-23 20:53:05 +0300

    fix: resolve syntax error missing closing bracket for children array

--- lib/widgets/voice_search_widget.dart
+++ lib/widgets/voice_search_widget.dart
@@ -519,6 +519,7 @@ class _VoiceSearchWidgetState extends State<VoiceSearchWidget>
                       Text('ابحث الآن',
                           style: TextStyle(
                               fontWeight: FontWeight.w800, fontSize: 15)),
+                    ],
                   ),
                 ),
                 ),
